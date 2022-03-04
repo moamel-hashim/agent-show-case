ae52c3b59a839e8d7ef1448095ced6012c18a6c3
moamel-hashim <muamal1997@gmail.com>
2022-03-03 17:15:03 -0800

    completed feature

--- js/data.js
+++ js/data.js
@@ -1,7 +1,40 @@
 /* exported data */
 
-const data = {
+var data = {
   view: 'home-page',
   favorite: [],
-  id: 0
+  id: 0,
+  character: [],
+  currentAgent: '',
+  gifLookUp: {
+    Breach: ['./images/breach/flashpoint.gif', './images/breach/fault line.gif', './images/breach/aftershock.gif', './images/breach/rolling thunder.gif'],
+    Raze: ['./images/raze/blast pack.gif', './images/raze/paint shells.gif', './images/raze/Boom bot.gif', './images/raze/showstopper.gif'],
+    Chamber: ['./images/chamber/RENDEZVOUS.gif', './images/chamber/trademark.gif', './images/chamber/headhunter.gif', './images/chamber/TOUR DE FORCE.gif'],
+    'KAY/O': ['./images/kayo/flash.gif', './images/kayo/zeropoint.gif', './images/kayo/fragment.gif', './images/kayo/nullcmd.gif'],
+    Skye: ['./images/skye/trailblazer.gif', './images/skye/guiding light.gif', './images/skye/regrowth.gif', './images/skye/seekers.gif'],
+    Cypher: ['./images/cypher/cyber cage.gif', './images/cypher/nerual theft.gif', './images/cypher/spycam.gif', './images/cypher/trapwire.gif'],
+    Sova: ['./images/sova/shock bolt.gif', './images/sova/recon bolt.gif', './images/sova/owl drone.gif', './images/sova/hunter\'s fury.gif'],
+    Killjoy: ['./images/killjoy/alarmbot.gif', './images/killjoy/turret.gif', './images/killjoy/nanoswarm.gif', './images/killjoy/lockdown.gif'],
+    Viper: ['./images/viper/poision cloud.gif', './images/viper/toxic screen.gif', './images/viper/snake bite.gif', './images/viper/vipers pit.gif'],
+    Phoenix: ['./images/phenix/curveball.gif', './images/phenix/hot hands.gif', './images/phenix/blaze.gif', './images/phenix/run it back.gif'],
+    Astra: ['./images/astra/nova pulse.gif', './images/astra/nebula.gif', './images/astra/gravity well.gif', './images/astra/astral form.gif'],
+    Brimstone: ['./images/brimstone/incendiary.gif', './images/brimstone/sky smoke.gif', './images/brimstone/stim beacon.gif', './images/brimstone/orbital strike.gif'],
+    Neon: ['./images/neon/relay bolt.gif', './images/neon/high gear.gif', './images/neon/fast lane.gif', './images/neon/overdrive.gif'],
+    Yoru: ['./images/yoru/blindside.gif', './images/yoru/gatecrash.gif', './images/yoru/fakeout.gif', './images/yoru/dimensional drift.gif'],
+    Sage: ['./images/sage/slow orb.gif', './images/sage/helaing orb.gif', './images/sage/barrier orb.gif', './images/sage/resurrection.gif'],
+    Reyna: ['./images/reyna/devour.gif', './images/reyna/dismiss.gif', './images/reyna/leer.gif', './images/reyna/empress.gif'],
+    Omen: ['./images/omen/paranoia.gif', './images/omen/dark cover.gif', './images/omen/shrouded step.gif', './images/omen/from the shadows.gif'],
+    Jett: ['./images/jett/updraft.gif', './images/jett/tailwind.gif', './images/jett/cloudburst.gif', './images/jett/blade storm.gif']
+  }
 };
+
+const previousDataJSON = localStorage.getItem('AJAX');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', BeforeUnloadHandler);
+function BeforeUnloadHandler(event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('AJAX', dataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
   // console.log('the status of xhr', xhr.status);
   // console.log('the response of xhr', xhr.response);
+  data.character = xhr.response.data;
+  renderAgents(xhr.response);
+});
+
+xhr.send();
+
+function renderAgents(agents) {
   const $li = document.createElement('li');
   $ul.appendChild($li);
   const $row = document.createElement('div');
@@ -14,11 +21,9 @@ xhr.addEventListener('load', function () {
   for (let i = 0; i < xhr.response.data.length; i++) {
     const $imgContainer = document.createElement('div');
     $imgContainer.setAttribute('class', 'img-container img-container-style');
-    $imgContainer.setAttribute('data-id', data.id);
     if (xhr.response.data[i].background !== null) {
       $imgContainer.style.backgroundImage = `url(${xhr.response.data[i].background})`;
     }
-    data.id++;
     const $img = document.createElement('img');
     $img.setAttribute('class', 'image');
     $img.setAttribute('data-id', data.id);
@@ -28,38 +33,124 @@ xhr.addEventListener('load', function () {
     const $agentTextContainer = document.createElement('div');
     $agentTextContainer.setAttribute('class', 'agent-text-container hidden');
     $agentTextContainer.setAttribute('data-id', data.id);
-    $agentTextContainer.setAttribute('data-visible', 'false');
     $imgContainer.appendChild($agentTextContainer);
     const $agentDescription = document.createElement('p');
     $agentDescription.setAttribute('class', 'open-sans');
+    data.id++;
     $agentDescription.textContent = xhr.response.data[i].description;
     $agentTextContainer.appendChild($agentDescription);
     const $images = document.querySelectorAll('.image');
     for (let j = 0; j < $images.length; j++) {
-      $images[i].addEventListener('click', function (event) {
+      $images[j].addEventListener('click', function (event) {
         const allTextContainer = document.querySelectorAll('.agent-text-container');
-        const imagesId = parseInt($images[i].getAttribute('data-id'));
+        const imagesId = parseInt($images[j].getAttribute('data-id'));
         for (let k = 0; k < allTextContainer.length; k++) {
-          const visibility = allTextContainer[k].getAttribute('data-visible');
           const textContainerIds = parseInt(allTextContainer[k].getAttribute('data-id'));
           if (imagesId === textContainerIds) {
             allTextContainer[k].classList.remove('hidden');
-          }
-          if (visibility === 'false') {
-            allTextContainer[k].setAttribute('data-visible', true);
           }
           allTextContainer[k].addEventListener('click', function (event) {
             if (imagesId === textContainerIds) {
               allTextContainer[k].classList.add('hidden');
             }
-            if (visibility === 'true') {
-              allTextContainer[k].setAttribute('data-visible', false);
-            }
           });
         }
       });
     }
+    const $buttonContainer = document.createElement('div');
+    $buttonContainer.setAttribute('class', 'button-container');
+    $imgContainer.appendChild($buttonContainer);
+    const $button = document.createElement('button');
+    $button.setAttribute('class', 'button');
+    $button.setAttribute('data-view', xhr.response.data[i].displayName);
+    $button.textContent = 'View Abilities';
+    $buttonContainer.appendChild($button);
   }
-});
+}
 
-xhr.send();
+$ul.addEventListener('click', handleButton);
+function handleButton(event) {
+  if (event.target.matches('button')) {
+    const $agent = document.querySelector('.agent');
+    const $viewValue = event.target.getAttribute('data-view');
+    data.view = $viewValue;
+    for (let i = 0; i < data.character.length; i++) {
+      if (data.character[i].displayName === data.view) {
+        $agent.setAttribute('data-view', data.character[i].displayName);
+        data.currentAgent = $agent.getAttribute('data-view');
+        renderAgentAbilities(data.character[i]);
+      }
+    }
+    switchView(data.view);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', handleDomContent);
+function handleDomContent(event) {
+  switchView(data.view);
+  const $agent = document.querySelector('.agent');
+  for (let i = 0; i < data.character.length; i++) {
+    if (data.character[i].displayName === data.currentAgent) {
+      $agent.setAttribute('data-view', data.currentAgent);
+      renderAgentAbilities(data.character[i]);
+    }
+  }
+}
+const $view = document.querySelectorAll('[data-view]');
+function switchView(viewName) {
+  data.view = viewName;
+  for (let i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') === viewName) {
+      $view[i].className = 'view';
+    } else {
+      $view[i].className = 'agent hidden';
+    }
+  }
+}
+
+const $a = document.querySelector('a');
+$a.addEventListener('click', handleHomePage);
+function handleHomePage(event) {
+  event.preventDefault();
+  switchView('home-page');
+}
+
+const $container = document.querySelector('.select-me');
+function renderAgentAbilities(abilities) {
+  const $dataView = document.querySelectorAll('[data-view]');
+  for (let i = 0; i < $dataView.length; i++) {
+    if (abilities.displayName !== $dataView[i].getAttribute('data-view')) {
+      $container.innerHTML = '';
+    }
+  }
+  const $h2 = document.createElement('h2');
+  $h2.textContent = abilities.displayName;
+  $h2.setAttribute('class', 'text-align-center proza-libre color-white');
+  $container.appendChild($h2);
+  const $row = document.createElement('div');
+  $row.setAttribute('class', 'row align-items-center');
+  $container.appendChild($row);
+  for (const keys in data.gifLookUp) {
+    if (keys === abilities.displayName) {
+      for (let i = 0; i < data.gifLookUp[keys].length; i++) {
+        const $gifContainer = document.createElement('div');
+        $gifContainer.setAttribute('class', 'gif-container');
+        $row.appendChild($gifContainer);
+        const $img = document.createElement('img');
+        $img.setAttribute('src', data.gifLookUp[keys][i]);
+        $gifContainer.appendChild($img);
+        const $columnHalf = document.createElement('div');
+        $columnHalf.setAttribute('class', 'column-half');
+        $row.appendChild($columnHalf);
+        const $h3 = document.createElement('h3');
+        $h3.setAttribute('class', 'ability-name color-white proza-libre');
+        $h3.textContent = abilities.abilities[i].displayName;
+        $columnHalf.appendChild($h3);
+        const $p = document.createElement('p');
+        $p.setAttribute('class', 'ability-description open-sans');
+        $p.textContent = abilities.abilities[i].description;
+        $columnHalf.appendChild($p);
+      }
+    }
+  }
+}
